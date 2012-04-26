450c839732509df2a733b5490be0f5d77e16850c
byuu <byuu@byuu.org>
2012-04-26 02:46:03 +0000

    Update to bsnes v088r01 release.
    * GB::CPU::Core -> Processor::LR35902
        * Processor::LR35902 -> jump table to switch table
        * GB::LCD -> GB::PPU
        * static frequency for DMG (no multiplication on clock ticks)
        * GB::PPU::interface->videoRefresh() moved outside scheduler (use
        host thread)
        * namespace NES  -> Famicom
        * namespace SNES -> SuperFamicom
        * namespace GB   -> GameBoy
        * namespace GBA  -> GameBoyAdvance
        * removed boot.rom writeout in GB::System
    
    **Wishlist for v089:**
    * libsnes++ (basically, cleaner C++ library interface to core for
    libretro to use)
    * internal filtering for matching hardware output effects (GB/GBA LCD
    blur, SNES pseudo-hires blending, possibly NES NTSC encoding, etc.)
    * internalize color conversion to -possibly- save an extra
    transformation ?
    * emulation improvements for NES, GB, GBC, GBA
    * speedups for GBA
    * complete GBA memory type database
    * GBA cheat codes
    * GBA cheat database ?
    * move NES::CPU (6502) to processor/
    * move SNES::CPU (65816) to processor/
    * move SNES::SMP (SPC700) to processor/
    * move SNES::SuperFX::Core (GSU) to processor/
    * all threads should be declared Class::Enter() { class.main(); }
    * nes -> fc, snes -> sfc ?
    * rewrite nall/array to be a static byte array rather than a POD-based
    vector class (get away from raw new / delete, where possible)

---
+++ gb/gb.hpp
@@ -0,0 +1,60 @@
+#ifndef GAMEBOY_HPP
+#define GAMEBOY_HPP
+
+#include <base/base.hpp>
+#include <processor/lr35902/lr35902.hpp>
+
+namespace GameBoy {
+  namespace Info {
+    static const char Name[] = "bgb";
+    static const unsigned SerializerVersion = 4;
+  }
+}
+
+/*
+  bgb - Game Boy, Super Game Boy, and Game Boy Color emulator
+  author: byuu
+  license: GPLv3
+  project started: 2010-12-27
+*/
+
+#include <libco/libco.h>
+
+namespace GameBoy {
+  struct Thread {
+    cothread_t thread;
+    unsigned frequency;
+    signed clock;
+
+    inline void create(void (*entrypoint)(), unsigned frequency) {
+      if(thread) co_delete(thread);
+      thread = co_create(65536 * sizeof(void*), entrypoint);
+      this->frequency = frequency;
+      clock = 0;
+    }
+
+    inline void serialize(serializer &s) {
+      s.integer(frequency);
+      s.integer(clock);
+    }
+
+    inline Thread() : thread(nullptr) {
+    }
+
+    inline ~Thread() {
+      if(thread) co_delete(thread);
+    }
+  };
+
+  #include <gb/memory/memory.hpp>
+  #include <gb/system/system.hpp>
+  #include <gb/scheduler/scheduler.hpp>
+  #include <gb/cartridge/cartridge.hpp>
+  #include <gb/cpu/cpu.hpp>
+  #include <gb/ppu/ppu.hpp>
+  #include <gb/apu/apu.hpp>
+  #include <gb/cheat/cheat.hpp>
+  #include <gb/video/video.hpp>
+};
+
+#endif
